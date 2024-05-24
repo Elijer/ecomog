c6a45eb0e79fc3200d47d26cce107bf0053adc05
Elijah Kennedy <elijahclimbs@gmail.com>
2024-05-24 18:31:58 -0400

    minimal svelte clientside directory

--- client/README.md
+++
@@ -1,2 +0,0 @@
-[Socket.io for the client](https://socket.io/docs/v4/client-installation/#from-npm)
-[Handling CORS](https://socket.io/docs/v4/handling-cors/)
--- client/main.js
+++ client/main.js
@@ -1,33 +1,4 @@
 import './style.css'
-import { io } from "socket.io-client";
+import { setupCounter } from './counter.js'
 
-const URL = "http://localhost:3000"
-// import { setupCounter } from './counter.js'
-
-const socket = io(URL);
-
-socket.on('connect', () => {
-  console.log('connected');
-  
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-    
-      // form.addEventListener('submit', (e) => {
-      //   e.preventDefault();
-      //   if (input.value) {
-      //     socket.emit('chat message', input.value);
-      //     input.value = '';
-      //   }
-      // });
-
-      // socket.on('chat message', (msg) => {
-      //   socket.emit("chat message", "tets")
-      //   const item = document.createElement('li');
-      //   item.textContent = msg;
-      //   messages.appendChild(item);
-      //   window.scrollTo(0, document.body.scrollHeight);
-      // });
-
-setupCounter(document.querySelector('#counter'))
+console.log("Hello from main.js")
