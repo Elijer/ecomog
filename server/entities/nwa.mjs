79b44cba014c3d095823033a2b8c169766197fd5
Elijah Kennedy <elijahclimbs@gmail.com>
2024-06-10 15:28:08 -0400

    added nwa file

---
+++ server/entities/nwa.mjs
@@ -0,0 +1,38 @@
+import Organism from './organism.mjs'
+import rgbHex from 'rgb-hex';
+import { CHANNELS } from '#root/saskanupe_constants.mjs';
+
+
+export default class Moss extends Organism {
+  constructor(
+    rows,
+    cols,
+    grid,
+    instances,
+    position,
+    startingEnergy,
+    generation = 1,
+    ){
+
+      super(rows, cols, grid, instances, position, CHANNELS.nwa, startingEnergy, generation)
+      this.rgb = [60, 180, 120]
+      this.type = "moss"
+      this.photosynthete = true
+    
+    }
+
+    getPyramidMaturity = () => {
+      return 2 * (.5 - Math.abs(this.maturityOutOfOne - .5))
+    }
+
+    reflect() {
+      return '#' + rgbHex(this.rgb[0], this.rgb[1], this.rgb[2], this.getPyramidMaturity() )
+    }
+
+    portableState(){
+      return {
+        ...super.portableState(),
+        color: this.reflect(),
+      }
+    }
+}
