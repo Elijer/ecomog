6cf3d897aa6941537134b04d340c09a9e0ca98b9
Elijah Kennedy <elijahclimbs@gmail.com>
2024-06-14 18:26:15 -0400

    removed comments

--- server/index.mjs
+++ server/index.mjs
@@ -4,53 +4,6 @@ import { Server } from 'socket.io';
 import GameInstance from './game.mjs';
 import { RULES, CHANNELS } from './saskanupe_constants.mjs';
 
-
-/*
-'use strict';
-
-const { networkInterfaces } = require('os');
-
-const nets = networkInterfaces();
-const results = Object.create(null); // Or just '{}', an empty object
-
-for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
-        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
-        if (net.family === familyV4Value && !net.internal) {
-            if (!results[name]) {
-                results[name] = [];
-            }
-            results[name].push(net.address);
-        }
-    }
-}
-    */
-
-// const getCurrentIp = () => {
-//   const nets = networkInterfaces();
-//   const results = Object.create(null); // Or just '{}', an empty object
-  
-//   for (const name of Object.keys(nets)) {
-//       for (const net of nets[name]) {
-//           // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-//           // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
-//           const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
-//           if (net.family === familyV4Value && !net.internal) {
-//               if (!results[name]) {
-//                   results[name] = [];
-//               }
-//               results[name].push(net.address);
-//           }
-//       }
-//   }
-//   console.log(results['en0'])
-//   return results['en0']
-// }
-
-console.log(process.env.IP)
-
 let frame = 0
 
 setInterval(() => {
